14568b68152065159a3303013bd5a44d855085f1
Serhii-Fesianov <littlefox06042000@gmail.com>
2023-12-03 17:09:45 +0200

    completed
    wait check

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems.map((item)=>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`
+).join('')
+  
+  galleryList.innerHTML = markup
+  
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    animationSlide: 100,
+  });
+
+  
+
+  
