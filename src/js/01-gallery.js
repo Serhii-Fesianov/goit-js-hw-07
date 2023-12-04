4d247ba320b68045d4304c5078eec3c7a6586eb2
Serhii-Fesianov <littlefox06042000@gmail.com>
2023-12-04 11:20:46 +0200

    Update 01-gallery.js
    fix first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,11 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-const galleryList = document.querySelector('.gallery')
+const galleryList = document.querySelector(".gallery");
 
-const markup = galleryItems.map((item)=>
-    `<li class="gallery__item">
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
     <a class="gallery__link" href="${item.original}">
       <img
         class="gallery__image"
@@ -13,39 +15,31 @@ const markup = galleryItems.map((item)=>
       />
     </a>
   </li>`
-).join('')
+  )
+  .join("");
 
 galleryList.innerHTML = markup;
 
-galleryList.addEventListener('click', (event)=>{
-  event.preventDefault()
-  if(event.target.nodeName !== 'IMG'){
-   return 
+const instance = basicLightbox.create(`<img src="" width="800" height="600">`, {
+  onShow: () => {
+    document.addEventListener("keydown", keyBordPress);
+  },
+  onClose: () => {
+    document.removeEventListener("keydown", keyBordPress);
+  },
+});
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
   }
-  const instance = basicLightbox.create(`
-  <img src="${event.target.dataset.source}" alt="${event.target.alt}">
-`)
+  instance.element().querySelector("img").src = event.target.dataset.source;
+  instance.show();
+});
 
-instance.show()
-
-document.addEventListener('keydown', onEscapeClick)
-function onEscapeClick(ev){
-if(ev.key !== 'Escape'){
-    return
+function keyBordPress(event) {
+  if (event.key === "Escape") {
+    instance.close();
+  }
 }
-instance.close()
-
-document.removeEventListener('keydown', onEscapeClick)
-}
-})
-
-// const instance = basicLightbox.create(`<img src="" width="800" height="600">`, {
-//     onShow: () => {
-//         document.addEventListener("keydown", onEscDown);
-//     },
-//     onClose: () => {
-//         document.removeEventListener("keydown", onEscDown);
-//     },
-// });
-
-
